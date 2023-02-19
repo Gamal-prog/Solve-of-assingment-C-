a9a38ffef312119358b1d5b8b753e4a4ea210e6d
Gamal-prog <124503906+Gamal-prog@users.noreply.github.com>
2023-02-19 15:00:21 +0600

    solve

---
+++ Гулливер.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int k, m;
+
+int main()
+{
+ cin >> k >> m;
+ cout << k * k * m;
+ return 0; 
+}
---
+++ Журавлики.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+long long s;
+
+int main()
+{
+ cin >> s;
+ cout << s / 6 << " " << 2 * (s / 6 + s / 6) << " " << s / 6;
+ return 0; 
+}
