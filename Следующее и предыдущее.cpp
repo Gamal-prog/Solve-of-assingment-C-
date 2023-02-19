aac2d53261c7a0f8f500f6de699ce7001f7f632a
Gamal-prog <124503906+Gamal-prog@users.noreply.github.com>
2023-02-19 13:33:26 +0600

    Solve

---
+++ Следующее и предыдущее.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+ cin >> n;
+ cout << "The next number for the number " << n << " is " << n + 1 << "." << endl;
+ cout << "The previous number for the number " << n << " is " << n - 1 << ".";
+ return 0;
+}
