c3bc8baf545229344b6325574194a84246ac909f
Gamal-prog <124503906+Gamal-prog@users.noreply.github.com>
2023-06-03 22:14:55 +0600

    Create Ферзь.cpp
    https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=2&id_topic=30&id_problem=186

---
+++ Ферзь.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int x1, x2, y, y2;
+
+int main()
+{
+    cin >> x1 >> y;
+    cin >> x2 >> y2;
+    if ( ( abs(x1 - x2) == abs(y - y2) ) or ( (x1 == x2) and (y != y2) ) or ( (x1 != x2) and (y == y2) ) )
+    {
+        cout << "YES"; 
+    }
+    else 
+    { 
+        cout << "NO";
+    }
+ return 0; 
+}
